40581427455e6c9d3990522864c8c57435d52a00
Chong-Li-nyu <31942702+Chong-Li-nyu@users.noreply.github.com>
2020-06-18 21:26:14 -0400

    Update MergeIntervals.java

--- intervals/MergeIntervals.java
+++ intervals/MergeIntervals.java
@@ -70,5 +70,52 @@ class Solution {
         ret[index++][1] = end;
         return Arrays.copyOfRange(ret, 0, index);
     }
-   
+}
+
+/*
+https://leetcode.com/problems/insert-interval/
+57. Insert Interval
+Hard
+
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+*/
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        // 找到合适的位置开始更新，最后记得放回这个区间
+        int n = intervals.length ;
+        int[][] ret = new int[ n + 1][2];
+        int start = newInterval[0];
+        int end = newInterval[1];
+        int index = 0;
+        
+        for(int i = 0; i < n; i++){
+            int curstart = intervals[i][0];
+            int curend = intervals[i][1];
+            if(start > curend){
+                ret[index++] = intervals[i];
+            } else {
+                if(end < curstart) {
+                    ret[index++] = new int[]{start, end};
+                    // copy remaining intervals
+                    for(int j = i; j < n; j++){
+                        ret[index++] = intervals[j];
+                    }
+                    return Arrays.copyOfRange(ret, 0, index);
+                } else { // start <= curend && end >= curstart
+                    start = Math.min(start, curstart);
+                    end = Math.max(end, curend);
+                }
+            }
+        }
+        ret[index++] = new int[]{start, end};
+        return Arrays.copyOfRange(ret, 0, index);
+    }
 }
